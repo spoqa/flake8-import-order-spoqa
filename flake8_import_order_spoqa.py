e64cefbfdf8298bfe761cbed415ef7935c5fd7b9
Hong Minhee <hong.minhee@gmail.com>
2017-01-31 03:53:06 +0900

    Initial commit

--- flake8_import_order_spoqa.py
+++ flake8_import_order_spoqa.py
@@ -1,6 +1,12 @@
+import ast
+import textwrap
 import unittest
 
-from flake8_import_order.styles import Style
+from flake8_import_order import (IMPORT_3RD_PARTY, IMPORT_APP,
+                                 IMPORT_APP_PACKAGE, IMPORT_APP_RELATIVE,
+                                 IMPORT_STDLIB)
+from flake8_import_order.checker import ImportOrderChecker
+from flake8_import_order.styles import Error, Style, lookup_entry_point
 
 __all__ = 'Spoqa',
 
@@ -11,12 +17,18 @@ class Spoqa(Style):
     def name_key(identifier):
         name = identifier.lstrip('_')
         hidden = name != identifier
-        if name.isupper():
+        if not name:
+            return 2, hidden
+        elif name.isupper():
             return 0, hidden  # constants e.g. CONSTANT_NAME
         elif name[0].isupper():
             return 1, hidden  # classes e.g. ClassName
         else:
             return 2, hidden  # normal variables e.g. normal_names
+
+    @staticmethod
+    def module_key(name):
+        return name.lower(), name
 
     @classmethod
     def sorted_names(cls, names):
@@ -24,11 +36,94 @@ class Spoqa(Style):
 
     @classmethod
     def import_key(cls, import_):
-        modules = [cls.name_key(module) for module in import_.modules]
+        modules = [cls.module_key(module) for module in import_.modules]
         names = [cls.name_key(name) for name in import_.names]
-        return (import_.type, import_.level, modules, names)
+        group = import_.type
+        if group in (IMPORT_APP, IMPORT_APP_PACKAGE):
+            group += IMPORT_APP_RELATIVE
+        return group, -import_.level, modules, names
+
+    def check(self):
+        for i in self.imports:
+            mod = i.modules[0]
+            if i.type == IMPORT_STDLIB and i.is_from:
+                yield Error(i.lineno, 'I901',
+                            'A standard library is imported using '
+                            '"from {0} import ..." statement. Should be '
+                            '"import {0}" instead.'.format(mod))
+            elif i.type == IMPORT_3RD_PARTY and not i.is_from:
+                yield Error(i.lineno, 'I902',
+                            'A third party library is imported using '
+                            '"import {0}" statement. Should be '
+                            '"from {0} import ..." instead.'.format(mod))
+            elif i.type in (IMPORT_APP, IMPORT_APP_PACKAGE) and not i.is_from:
+                yield Error(i.lineno, 'I902',
+                            'An app module is imported using "import {0}"'
+                            ' statement. Should be "from {0} import ..."'
+                            ' instead.'.format(mod))
+        for error in super(Spoqa, self).check():
+            yield error
 
 
 class TestCase(unittest.TestCase):
-    def test_sample(self):
-        self.assertEquals(1, 2)
+
+    def make_test(expected_error_codes, code):
+        def test_func(self):
+            data = textwrap.dedent(code)
+            tree = ast.parse(data, '<stdin>')
+            checker = ImportOrderChecker(None, tree)
+            checker.lines = data.splitlines(True)
+            checker.options = {
+                'application_import_names': ['spoqa', 'tests'],
+                'import_order_style': lookup_entry_point('spoqa'),
+            }
+            actual_error_codes = frozenset(error.code
+                                           for error in checker.check_order())
+            self.assertEquals(frozenset(expected_error_codes),
+                              actual_error_codes)
+        return test_func
+
+    test_valid_1 = make_test([], '''
+        import datetime
+        import sys
+
+        from pkg_resources import (SOURCE_DIST, EntryPoint, Requirement,
+                                   get_provider)
+
+        from ...deepest import a
+        from ..deeper import b
+        from .a import this, that
+        from .z import This, That
+        from spoqa import something
+    ''')
+
+    test_constants_must_be_former_than_classes = make_test(['I101'], '''
+        from pkg_resources import EntryPoint, SOURCE_DIST
+    ''')
+
+    test_classes__must_be_former_than_normal_names = make_test(['I101'], '''
+        from pkg_resources import get_provider, Requirement
+    ''')
+
+    test_deeper_relative_imports_must_be_latter = make_test(['I100'], '''
+        from ..deeper import b
+        from ...deepest import a
+    ''')
+
+    test_relative_imports_must_be_former_than_app_imports = \
+        make_test(['I100'], '''
+            from spoqa import b
+            from .rel import a
+        ''')
+
+    test_standard_libraries_must_not_be_from_import = make_test(['I901'], '''
+        from sys import argv
+    ''')
+
+    test_3rd_parties_must_be_from_import = make_test(['I902'], '''
+        import pkg_resources
+    ''')
+
+    test_apps_must_be_from_import = make_test(['I902'], '''
+        import spoqa
+    ''')
